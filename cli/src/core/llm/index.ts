b8bfc7c50b95b498c81fce040f365ce69f3aa872
Aditya <adityasai3230@gmail.com>
2026-04-14 16:04:49 +0530

    feat(llm): multi-provider client - OpenAI, Anthropic, Google, Azure

---
+++ cli/src/core/llm/index.ts
@@ -0,0 +1,5 @@
+export * from "./utils.js";
+export * from "./prompts.js";
+export * from "./client.js";
+export { createProvider, getClient, setClient } from "./client.js";
+export type { LLMProvider, LLMResponse, LLMOptions, Message } from "./client.js";
